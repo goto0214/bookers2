1c518af7aef8da742c7bf1b2b599c3aad620a608
goto0214 <yuusuke0214goto1997@outlook.jp>
2021-12-09 15:54:37 +0000

    [finish]4-10

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,4 @@
 class BooksController < ApplicationController
-  before_action :baria_user, only: [:edit, :update, :destroy]
 
   def index
     @books = Book.page(params[:page]).reverse_order
@@ -63,12 +62,5 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title, :body)
   end
-
-  def baria_user
-    unless Book.find(params[:id]).user.id.to_i == current_user.id
-      redirect_to user_path(current_user)
-    end
-  end
-
 
 end
