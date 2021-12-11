1d549e7c9e39f28b66e981067dec254e76a7a3b8
goto0214 <yuusuke0214goto1997@outlook.jp>
2021-12-11 08:51:11 +0000

    [Fix]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,11 @@ class ApplicationController < ActionController::Base
 
 
   def after_sign_in_path_for(resource)
-    user_path(@user.id)
+    user_path(current_user)
   end
 
   def after_sign_up_path_for(resource)
-    user_path(@user.id)
+    user_path(current_user)
   end
 
   def after_sign_out_path_for(resource)
